3802f1d01ce9690067b70d9ce8e9739b475e92a6
Vladimir Popov <vladapopov02@gmail.com>
2024-01-27 21:43:16 +0100

    feat: add Config.kt class for getting configuration variables

--- src/main/kotlin/utils/Config.kt
+++ src/main/kotlin/utils/Config.kt
@@ -1,4 +1,40 @@
 package org.example.utils
 
+import io.ktor.serialization.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import java.io.File
+
 class Config {
+    var baseUrl: String? = null
+        private set
+        get() {
+            if (field == null)
+                parseConfigFile()
+            return field!!
+        }
+
+    var apiKey: String? = null
+        private set
+        get() {
+            if (field == null)
+                parseConfigFile()
+            return field!!
+        }
+
+    private fun parseConfigFile() {
+        val configFileText: String = File("config.json").readText(Charsets.UTF_8)
+        val jsonConfig: Map<String, JsonElement> = Json.parseToJsonElement(configFileText).jsonObject
+
+        baseUrl = extractValue("BASE_URL", jsonConfig)
+        apiKey = extractValue("API_KEY", jsonConfig)
+    }
+
+    private fun extractValue(property: String, jsonMap: Map<String, JsonElement>): String {
+        if (jsonMap.containsKey(property))
+            return jsonMap[property].toString()
+        else
+            throw JsonConvertException("There is no $property property in config file!")
+    }
 }
